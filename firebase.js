143681d05bee8eed966c54b40f9a0ced6c3e9d1f
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-05-15 22:34:22 +0900

    first commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyapIc2LJhDoE4dOB81A2c-uPumqUsi0w",
+  authDomain: "inquiry-a4126.firebaseapp.com",
+  projectId: "inquiry-a4126",
+  storageBucket: "inquiry-a4126.appspot.com",
+  messagingSenderId: "393682654049",
+  appId: "1:393682654049:web:3934a740cc05043b232b20"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
